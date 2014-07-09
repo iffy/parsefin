f3bae7fc8fa2fee5398a4f6791375a4636cfca9c
Matt Haggard <haggardii@gmail.com>
2014-07-09 14:47:50 -0600

    First failing test

---
+++ parsefin/test/test_ofx.py
@@ -0,0 +1,11 @@
+from twisted.trial.unittest import TestCase
+
+
+from parsefin.ofx import OFXTransactionParser
+
+
+class OFXTransactionParserTest(TestCase):
+
+
+    def test_fromFile(self):
+        pass
